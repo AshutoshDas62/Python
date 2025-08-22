2551b5475e99fc029802c8c970a9302471eabec5
AshutoshDas62 <ashutoshkrdas2@gmail.com>
2025-08-22 19:36:08 +0530

    # 4) wap the takes an integer as input and tell the wheater the no the consize of leep year or

---
+++ 4thprg.py
@@ -0,0 +1,6 @@
+# 4) wap the takes an integer as input and tell the wheater the no the consize of leep year or 
+year = int(input("enter year"))
+if year%4==0:
+    print("leep year")
+else:
+    print("non year ")
